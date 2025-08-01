f84c113a42e72092c305c752ca8caa2f3f589cd3
mustafaxcakir <98mustafacan@gmail.com>
2025-08-01 16:05:28 +0300

    Refactor migration to safely drop 'about_privacy' column from users table
    - Added a check to ensure the 'about_privacy' column exists before attempting to drop it, preventing potential errors during migration.

--- database/migrations/2025_07_27_101519_remove_about_privacy_from_users_table.php
+++ database/migrations/2025_07_27_101519_remove_about_privacy_from_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('about_privacy');
+            if (Schema::hasColumn('users', 'about_privacy')) {
+                $table->dropColumn('about_privacy');
+            }
         });
     }
 
